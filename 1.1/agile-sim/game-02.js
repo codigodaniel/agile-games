b70338edc122e4473497a461a588d686b0dafa21
Daniel <daniel.ceillan@yahoo.es>
2021-04-30 15:50:07 +0200

    Config Sim first version

---
+++ 1.1/agile-sim/game-02.js
@@ -0,0 +1,29 @@
+//***************
+// Game  02
+//***************
+
+/* Dependencies
+*/
+
+class G02_SectionElement{
+    section = 2;
+}
+
+class G02_ConfigSim extends G02_SectionElement{
+
+    reset_board(){
+
+    }
+    init_html(){
+
+    }
+
+}
+
+//*******************
+//      SETUP
+//*******************
+
+var G02_game = new G02_ConfigSim();
+
+G02_game.reset_board();
